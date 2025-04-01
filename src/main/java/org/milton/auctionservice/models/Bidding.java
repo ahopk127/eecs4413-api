3f5cfdcdcb6b5a60f4e7656048c6623b064db17d
harnain <harnain33@gmail.com>
2025-04-01 17:24:08 -0400

    commenting bidding

--- src/main/java/org/milton/auctionservice/models/Bidding.java
+++ src/main/java/org/milton/auctionservice/models/Bidding.java
@@ -1,50 +1,60 @@
-package org.milton.auctionservice.models;
+package org.milton.auctionservice.models; //package declaration - indicating that this file belongs to the models layer of the application
 
-import jakarta.persistence.*;
+import jakarta.persistence.*; // importing JPA (Java Persistence API) annotations from java persistence - used to map this class to a database table
 
-@Entity
-public class Bidding {
+@Entity //marks this class as a JPA entity, meaning it will be mapped to a database table
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long  id;
-    @ManyToOne
-    @JoinColumn(name = "bidder_id")
-    private User bidder;
-    @ManyToOne
-    @JoinColumn(name = "item_id")
-    private Item item;
-    private Double bidingPrice;
+/*
+* this class defines a bidding entity linked to a user/bidder and an item
+* uses JPA annotations to map class to a relational database
+* stores the bid amount in biddingPrice
+* getter and setter methods allow controlled access
+*/
+public class Bidding { //declares the class Bidding - representing a bid in the auction system
+
+    @Id //marks this field as the primary key of the entity
+    @GeneratedValue(strategy = GenerationType.AUTO) //automatically generates unique IDs for each bid, allowing the database to decide the generation strategy
+    private Long  id; //stores the unique identifier for each bid
+
+    @ManyToOne //represents a many-to-one relationship - one user can place many bids
+    @JoinColumn(name = "bidder_id") //specifies that the bidder_id column in the table will store the foreign key linking to User
+    private User bidder; //Stores the user who placed the bid
+
+    @ManyToOne //represents a many-to-one relationship - a single item can receive many bids
+    @JoinColumn(name = "item_id") //links the bidding.item_id column to the Item entity's primary key
+    private Item item; //Stores the item being bid on
+
+    private Double bidingPrice; //Stores the bid amount placed by the user
 
     public Long getId() {
-        return id;
+        return id; //returns the bid ID - primary key
     }
 
     public void setId(Long id) {
-        this.id = id;
+        this.id = id; //allows setting the bid ID
     }
 
     public User getBidder() {
-        return bidder;
+        return bidder; //returns the user who placed the bid
     }
 
     public void setBidder(User bidder) {
-        this.bidder = bidder;
+        this.bidder = bidder; //sets the bidder information
     }
 
     public Item getItem() {
-        return item;
+        return item; //returns the item being bid on
     }
 
     public void setItem(Item item) {
-        this.item = item;
+        this.item = item; //sets the item being bid on
     }
 
     public Double getBidingPrice() {
-        return bidingPrice;
+        return bidingPrice; //returns the bid amount
     }
 
     public void setBidingPrice(Double bidingPrice) {
-        this.bidingPrice = bidingPrice;
+        this.bidingPrice = bidingPrice; //sets the bid amount 
     }
 }
